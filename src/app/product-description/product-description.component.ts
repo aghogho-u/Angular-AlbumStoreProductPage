12cfd34263402aad82034aceb18d85e023a75c20
Aghogho <auwejeyan@gmail.com>
2019-10-30 18:49:16 +0100

    Using interfaces to describe data

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { ProductPageComponent } from 'app/product-page/product-page.component';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
@@ -8,7 +9,7 @@ import { ProductPageComponent } from 'app/product-page/product-page.component';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
